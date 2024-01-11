318e603bea7237a6741dd654e5b9ef811b7ee578
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-12 02:16:16 +0530

    ok

--- E-commers front-end/src/App.jsx
+++ E-commers front-end/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './Componets/Login';
 import RegistrationPage from './Componets/Register';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Homerots from './Componets/homerots';
 
 const App = () => {
   return (<>
@@ -12,6 +13,8 @@ const App = () => {
             element={<Login />} />
           <Route path='register'
             element={<RegistrationPage />} />
+          <Route path='home'
+            element={<Homerots />} />
         </Routes>
       </BrowserRouter>
 
---
+++ E-commers front-end/src/Componets/homerots.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homerots() {
+  return (
+    <div>
+      <h1>Home routes</h1>
+    </div>
+  )
+}
+
+export default Homerots;
