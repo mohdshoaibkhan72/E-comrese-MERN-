c28a2607eecc71e0eae3bae89c5cfcda5168d22d
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-16 13:46:11 +0530

    done know exam time

--- E-commers front-end/src/Componets/Addproduct/GetProduct.jsx
+++ E-commers front-end/src/Componets/Addproduct/GetProduct.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -7,11 +7,11 @@ const ProductList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/getproducts');
+        const response = await axios.get("http://localhost:8000/getproducts");
         console.log(response.data);
         setProducts(response.data);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error("Error fetching products:", error);
       }
     };
 
@@ -23,30 +23,28 @@ const ProductList = () => {
   };
 
   return (
-    <div>
-      <div className="card-container">
-        {products.map((product) => (
-          <div key={product.productId} className="card"   >
-            <img
-              src={`http://localhost:8000/${product.productPhoto.filename}`} 
-              alt={product.productName}
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <h5 className="card-title">{product.productName}</h5>
-              <p className="card-text">{product.productDescription}</p>
-              <p className="card-text">Price: ${product.productPrice}</p>
-              <button
-                className="btn btn-primary"
-                onClick={() => handleAddToCart(product.productId)}
-              >
-                Add to Cart
-              </button>
+    <>
+      <div className="cards">
+        <div className="card-container ">
+          {products.map((product) => (
+            <div key={product.productId} className="card cardbox">
+              <div> <img
+                src={`http://localhost:8000/${product.productPhoto.filename}`}
+                alt={product.productName}
+                className="card-img-top"/> </div>
+              <div className="card-body">
+                <h5 className="card-title">{product.productName}</h5>
+                <p className="card-text">{product.productDescription}</p>
+                <p className="card-text">Price: ${product.productPrice}</p>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleAddToCart(product.productId)}>Add to Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
