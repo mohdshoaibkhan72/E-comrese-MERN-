07b0b8b8594b144c47018ebe263ab8073fb68771
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-03-14 23:02:33 +0530

    db connection atals

--- Server/connection/DbConnection.js
+++ Server/connection/DbConnection.js
@@ -3,7 +3,9 @@ const connectDB = async () => {
   try {
     await mongoose
 
-      .connect("mongodb://127.0.0.1:27017/mydatabase")
+      .connect(
+        "mongodb+srv://mohdshoaib72:C3eNKTNQXB7bPoJN@cluster0.gm9smjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      )
       .then(console.log("mongose is connected suucesfulluly "));
   } catch (error) {
     console.log(error);
